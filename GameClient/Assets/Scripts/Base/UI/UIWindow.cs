63ce5ee31f23da56281fa8b26c90363d12bb095b
xiangchunsong520 <xiangchunsong520@gmail.com>
2017-03-31 18:45:31 +0800

    2017-03-31 02

---
+++ GameClient/Assets/Scripts/Base/UI/UIWindow.cs
@@ -0,0 +1,51 @@
+﻿/*
+auth: Xiang ChunSong
+purpose:
+*/
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIObject
+{
+    GameObject _gameObject;
+
+    protected Transform GetChildTransform(string childPath)
+    {
+        return _gameObject.transform.FindChild(childPath);
+    }
+
+    protected GameObject GetChildGameObject(string childPath)
+    {
+        Transform ts = GetChildTransform(childPath);
+        if (ts)
+            return ts.gameObject;
+
+        return null;
+    }
+
+    protected T GetComponen<T>() where T : Component
+    {
+        return _gameObject.GetComponent<T>();
+    }
+
+    protected T GetChildComponent<T>(string childPath) where T : Component
+    {
+        Transform ts = GetChildTransform(childPath);
+        if (ts)
+            return ts.GetComponent<T>();
+
+        return null;
+    }
+}
+
+public class UIChildWindow
+{
+
+}
+
+public class UIWindow
+{
+
+}
