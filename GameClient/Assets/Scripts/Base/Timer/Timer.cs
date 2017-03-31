68971ec963ad8ef1d075858f17de162c59e31d75
xiangchunsong520 <xiangchunsong520@gmail.com>
2017-03-31 15:53:20 +0800

    1

--- GameClient/Assets/Scripts/Base/Timer/Timer.cs
+++ GameClient/Assets/Scripts/Base/Timer/Timer.cs
@@ -3,21 +3,103 @@ auth: Xiang ChunSong
 purpose:
 */
 
-using UnityEngine;
-using System.Collections;
+using System;
 
-public class Timer : MonoBehaviour
+namespace Base
 {
-
-    // Use this for initialization
-    void Start()
+    public class Timer
     {
+        Action<object[]> _callback;
+        object[] _args;
+        
+        public virtual bool CheckExecute()
+        {
+            throw new NotImplementedException();
+        }
 
+        public Timer(Action<object[]> callback, object[] args)
+        {
+            _callback = callback;
+            _args = args;
+        }
+
+        protected void Execute()
+        {
+            if (_callback != null)
+            {
+                try
+                {
+                    _callback(_args);
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public class DelayTimer : Timer
     {
+        DateTime _executeTime;
 
+        public DelayTimer(float delayTime, Action<object[]> callback, object[] args) : base(callback, args)
+        {
+            _executeTime = DateTime.Now + new TimeSpan((long)(TimeSpan.TicksPerSecond * delayTime));
+        }
+
+        public override bool CheckExecute()
+        {
+            if (DateTime.Now >= _executeTime)
+            {
+                Execute();
+                return true;
+            }
+            return true;
+        }
+    }
+
+    public class RepeatTimer : Timer
+    {
+        DateTime _executeTime;
+        TimeSpan _repeatTime;
+
+        public RepeatTimer(float delayTime, float repeatTime, Action<object[]> callback, object[] args) : base(callback, args)
+        {
+            _executeTime = DateTime.Now + new TimeSpan((long)(TimeSpan.TicksPerSecond * delayTime));
+            _repeatTime = new TimeSpan((long)(TimeSpan.TicksPerSecond * repeatTime)); ;
+        }
+
+        public override bool CheckExecute()
+        {
+            if (DateTime.Now >= _executeTime)
+            {
+                Execute();
+                _executeTime = _executeTime + _repeatTime;
+            }
+            return false;
+        }
+    }
+
+    public class FrameTimer : Timer
+    {
+        int _delayFrame;
+        int _curFrame;
+
+        public FrameTimer(int delayFrame, Action<object[]> callback, object[] args) : base(callback, args)
+        {
+            _delayFrame = delayFrame;
+            _curFrame = 0;
+        }
+
+        public override bool CheckExecute()
+        {
+            if (++_curFrame >= _delayFrame)
+            {
+                Execute();
+                return true;
+            }
+            return false;
+        }
     }
 }
---
+++ GameClient/Assets/Scripts/Base/Timer/TimerManager.cs
@@ -0,0 +1,71 @@
+﻿/*
+auth: Xiang ChunSong
+purpose:
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace Base
+{
+    public class TimerManager : Singleton<TimerManager>
+    {
+        List<Timer> _timers = new List<Timer>();
+        List<Timer> _removed = new List<Timer>();
+
+        static object threadLoack = new object();
+
+        public void Update()
+        {
+            for (int i = 0; i < _timers.Count;)
+            {
+                Timer timer = _timers[i];
+                if (timer == null)
+                {
+                    _timers.RemoveAt(i);
+                }
+                else if (_removed.Contains(timer) || timer.CheckExecute())
+                {
+                    _timers.RemoveAt(i);
+                }
+                else
+                {
+                    ++i;
+                }
+            }
+            lock (threadLoack)
+            {
+                _removed.Clear();
+            }
+        }
+
+        public Timer AddDelayTimer(float delayTime, Action<object[]> callback, params object[] args)
+        {
+            Timer timer = new DelayTimer(delayTime, callback, args);
+            _timers.Add(timer);
+            return timer;
+        }
+
+        public Timer AddRepeatTimer(float delayTime, float repeatTime, Action<object[]> callback, params object[] args)
+        {
+            Timer timer = new RepeatTimer(delayTime, repeatTime, callback, args);
+            _timers.Add(timer);
+            return timer;
+        }
+
+        public Timer AddFarmeTimer(int farmeCount, Action<object[]> callback, params object[] args)
+        {
+            Timer timer = new FrameTimer(farmeCount, callback, args);
+            _timers.Add(timer);
+            return timer;
+        }
+
+        public void RemoveTimer(Timer timer)
+        {
+            lock (threadLoack)
+            {
+                _removed.Add(timer);
+            }
+        }
+    }
+}
